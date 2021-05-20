d32181c6ba435f54e0d4b9f95d6b66d7575313ff
Asanga Udara <70792747+asangaudara@users.noreply.github.com>
2021-05-20 13:14:24 +0530

    Update _plugin.js

--- plugins/_plugin.js
+++ plugins/_plugin.js
@@ -1,9 +1,9 @@
-/*
-# Exclusively from SL-Android project 
+/* # Exclusively from danuma project 
 # Do not use this fore any commercial thing
 # If you abuse thais bot we wil kick you from bot 
 # Do not edit (Respect to the Devaoloper) 
-# All rights reserved ©Asanga_Udara
+# All rights reserved ©Lasiya @lasiya99X t.me/lasiya99X
+# Get more about devaoloper https://lasiya.ml
 */
 
 const Asena = require('../events');
